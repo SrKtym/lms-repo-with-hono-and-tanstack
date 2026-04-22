db7b7feb660c422075fa19aa918bdbee5d8fedfc
SrKtym <frmywrkplc@gmail.com>
2026-04-22 17:25:01 +0900

    fix: queryClientの修正

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,10 @@
 import { Loader } from "@lms-repo/ui/components/loader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
+
+const queryClient = new QueryClient();
 
 const router = createRouter({
 	routeTree,
@@ -23,5 +26,9 @@ if (!rootElement) {
 
 if (!rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
-	root.render(<RouterProvider router={router} />);
+	root.render(
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	);
 }
