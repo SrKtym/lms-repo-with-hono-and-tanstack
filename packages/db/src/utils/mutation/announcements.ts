8c407708271e015fa6f17baaad59a59824f0d0fb
SrKtym <frmywrkplc@gmail.com>
2026-05-03 16:31:11 +0900

    fix: ステータスコード修正

--- packages/db/src/utils/mutation/announcements.ts
+++ packages/db/src/utils/mutation/announcements.ts
@@ -6,26 +6,26 @@ import type { Announcements } from "../../types";
 export async function createAnnouncements(announcementsData: Announcements) {
 	try {
 		await db.insert(announcements).values(announcementsData);
-		return { message: "アナウンスメントの作成に成功しました。", status: 201 }
+		return { message: "アナウンスメントの作成に成功しました。", status: 201 };
 	} catch {
-		return { message: "アナウンスメントの作成に失敗しました。", status: 400 }
+		return { message: "アナウンスメントの作成に失敗しました。", status: 500 };
 	}
 }
 
 export async function updateAnnouncements(announcementsData: Announcements) {
 	try {
 		await db.update(announcements).set(announcementsData);
-		return { message: "アナウンスメントの更新に成功しました。", status: 200 }
+		return { message: "アナウンスメントの更新に成功しました。", status: 200 };
 	} catch {
-		return { message: "アナウンスメントの更新に失敗しました。", status: 400 }
+		return { message: "アナウンスメントの更新に失敗しました。", status: 500 };
 	}
 }
 
 export async function deleteAnnouncements(id: string) {
 	try {
 		await db.delete(announcements).where(eq(announcements.id, id));
-		return { message: "アナウンスメントの削除に成功しました。", status: 200 }
+		return { message: "アナウンスメントの削除に成功しました。", status: 200 };
 	} catch {
-		return { message: "アナウンスメントの削除に失敗しました。", status: 400 }
+		return { message: "アナウンスメントの削除に失敗しました。", status: 500 };
 	}
 }
--- packages/db/src/utils/mutation/notifications.ts
+++ packages/db/src/utils/mutation/notifications.ts
@@ -6,18 +6,18 @@ import type { Notifications } from "../../types";
 export async function createNotifications(notificationsData: Notifications) {
 	try {
 		await db.insert(notifications).values(notificationsData);
-		return { message: "通知の作成に成功しました。", status: 201 }
+		return { message: "通知の作成に成功しました。", status: 201 };
 	} catch {
-		return { message: "通知の作成に失敗しました。", status: 400 }
+		return { message: "通知の作成に失敗しました。", status: 500 };
 	}
 }
 
 export async function deleteNotification(notificationId: string) {
 	try {
 		await db.delete(notifications).where(eq(notifications.id, notificationId));
-		return { message: "通知の削除に成功しました。", status: 200 }
+		return { message: "通知の削除に成功しました。", status: 200 };
 	} catch {
-		return { message: "通知の削除に失敗しました。", status: 400 }
+		return { message: "通知の削除に失敗しました。", status: 500 };
 	}
 }
 
@@ -27,20 +27,18 @@ export async function markNotificationAsRead(notificationId: string) {
 			.update(notifications)
 			.set({ isRead: true })
 			.where(eq(notifications.id, notificationId));
-		return { message: "通知の既読処理に成功しました。", status: 200 }
+		return { message: "通知の既読処理に成功しました。", status: 200 };
 	} catch {
-		return { message: "通知の既読処理に失敗しました。", status: 400 }
+		return { message: "通知の既読処理に失敗しました。", status: 500 };
 	}
 }
 
 export async function markAllNotificationsAsRead() {
 	try {
-		await db
-			.update(notifications)
-			.set({ isRead: true });
-		return { message: "通知の既読処理に成功しました。", status: 200 }
+		await db.update(notifications).set({ isRead: true });
+		return { message: "通知の既読処理に成功しました。", status: 200 };
 	} catch {
-		return { message: "通知の既読処理に失敗しました。", status: 400 }
+		return { message: "通知の既読処理に失敗しました。", status: 500 };
 	}
 }
 
