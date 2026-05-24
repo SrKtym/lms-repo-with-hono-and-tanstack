21ac7a416a3a9dfb8d781db5a07b5f6713e4540d
SrKtym <frmywrkplc@gmail.com>
2026-05-25 00:06:40 +0900

    docs: 環境変数の追加

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -14,6 +14,10 @@ export const env = createEnv({
 		DATABASE_URL_FOR_DOCKER: z.string().min(1),
 		BETTER_AUTH_SECRET: z.string().min(32),
 		BETTER_AUTH_URL: z.url(),
+		GOOGLE_CLIENT_SECRET: z.string().min(32),
+		GOOGLE_CLIENT_ID: z.string().min(1),
+		GITHUB_CLIENT_SECRET: z.string().min(32),
+		GITHUB_CLIENT_ID: z.string().min(1),
 		RESEND_API_KEY: z.string().min(1),
 		CORS_ORIGIN: z.url(),
 		NODE_ENV: z
