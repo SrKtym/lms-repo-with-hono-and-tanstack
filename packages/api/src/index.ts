8063e249c886ccfe5bb107453785569028658c6b
SrKtym <frmywrkplc@gmail.com>
2026-05-25 00:37:37 +0900

    docs: studentsRoute, commentsRoute, settingsRouteの追加

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import { announcementsRoute } from "./routes/announcements";
 import { assignmentsRoute } from "./routes/assignments";
+import { commentsRoute } from "./routes/comments";
 import { coursesRoute } from "./routes/courses";
 import { notificationsRoute } from "./routes/notifications";
 import { schedulesRoute } from "./routes/schedules";
+import { settingsRoute } from "./routes/settings";
 import { studentsRoute } from "./routes/students";
 import { submissionsRoute } from "./routes/submissions";
 
@@ -15,6 +17,8 @@ export const fullRoutes = new Hono()
 	.route("/announcements", announcementsRoute)
 	.route("/submissions", submissionsRoute)
 	.route("/notifications", notificationsRoute)
-	.route("/students", studentsRoute);
+	.route("/students", studentsRoute)
+	.route("/comments", commentsRoute)
+	.route("/settings", settingsRoute);
 
 export type FullRoutes = typeof fullRoutes;
