4454568d6b07673720259ce3af0601a57f4d558c
SrKtym <frmywrkplc@gmail.com>
2026-06-28 12:46:03 +0900

    chore: 微修正

--- packages/db/src/migrations/0001_overjoyed_gorgon.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "students" ALTER COLUMN "required_credits" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "comments" ADD COLUMN "assignment_id" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "comments" ADD CONSTRAINT "comments_assignment_id_assignments_id_fk" FOREIGN KEY ("assignment_id") REFERENCES "public"."assignments"("id") ON DELETE cascade ON UPDATE no action;
