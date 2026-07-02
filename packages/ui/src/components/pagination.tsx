bda1cb6985ba8831045aa7692fc7398b7fd57596
SrKtym <frmywrkplc@gmail.com>
2026-07-02 18:27:31 +0900

    refactor: 微修正

--- packages/ui/src/components/pagination.tsx
+++ packages/ui/src/components/pagination.tsx
@@ -20,28 +20,35 @@ export function DefaultPagination({
 	const endItem = Math.min(currentPage * itemsPerPage, totalItems);
 	const displayStartItem = Math.min(startItem, endItem);
 
+	// ページ番号を計算する関数
 	const getPageNumbers = () => {
 		const pages = [];
 		const maxVisiblePages = 5;
 
+		// totalPagesがmaxVisiblePages以下なら全て表示
 		if (totalPages <= maxVisiblePages) {
 			Array.from({ length: totalPages }, (_, i) => i + 1).forEach((i) => {
 				pages.push(i);
 			});
 		} else {
+			// currentPageが3以下（前半）なら1,2,3,4,...,totalPages
 			if (currentPage <= 3) {
 				Array.from({ length: 4 }, (_, i) => i + 1).forEach((i) => {
 					pages.push(i);
 				});
 				pages.push("ellipsis");
 				pages.push(totalPages);
-			} else if (currentPage >= totalPages - 2) {
+			}
+			// currentPageがtotalPages-2の値以上（後半）なら1,...,totalPages-3,totalPages-2,totalPages-1,totalPages
+			else if (currentPage >= totalPages - 2) {
 				pages.push(1);
 				pages.push("ellipsis");
 				Array.from({ length: 4 }, (_, i) => totalPages - 3 + i).forEach((i) => {
 					pages.push(i);
 				});
-			} else {
+			}
+			// その他の場合は1,...,currentPage-1,currentPage,currentPage+1,...,totalPages
+			else {
 				pages.push(1);
 				pages.push("ellipsis");
 				pages.push(currentPage - 1);
@@ -60,15 +67,18 @@ export function DefaultPagination({
 				{displayStartItem} to {endItem} of {totalItems}
 			</Pagination.Summary>
 			<Pagination.Content>
+				{/* 前のページ */}
 				<Pagination.Item>
 					<Pagination.Previous
 						isDisabled={currentPage === 1}
 						onPress={() => onPageChange(currentPage - 1)}
 					>
 						<Pagination.PreviousIcon />
-						<span>前のページ</span>
+						<p>前のページ</p>
 					</Pagination.Previous>
 				</Pagination.Item>
+
+				{/* ページ番号 */}
 				{getPageNumbers().map((page, index) =>
 					page === "ellipsis" ? (
 						<Pagination.Item key={`ellipsis-${index}`}>
@@ -85,12 +95,14 @@ export function DefaultPagination({
 						</Pagination.Item>
 					),
 				)}
+
+				{/* 次のページ */}
 				<Pagination.Item>
 					<Pagination.Next
 						isDisabled={currentPage === totalPages}
 						onPress={() => onPageChange(currentPage + 1)}
 					>
-						<span>次のページ</span>
+						<p>次のページ</p>
 						<Pagination.NextIcon />
 					</Pagination.Next>
 				</Pagination.Item>
