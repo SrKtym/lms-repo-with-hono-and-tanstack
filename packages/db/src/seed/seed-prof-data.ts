f498badfe0d2902a1934ee8495e9d09155c9892e
SrKtym <frmywrkplc@gmail.com>
2026-07-26 19:33:11 +0900

    Delete packages/db/src/seed directory

--- packages/db/src/seed/seed-prof-data.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "..";
-import { mockProfessors } from "../mock/mock-prof-data";
-import { user } from "../schema";
-
-export async function seedUserData() {
-	await db.insert(user).values(mockProfessors).onConflictDoNothing();
-}
