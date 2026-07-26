a1f4e66a73ab87a821e325032e7c8fef97860cde
SrKtym <frmywrkplc@gmail.com>
2026-07-26 19:33:43 +0900

    Delete packages/db/src/seed.ts

--- packages/db/src/seed.ts
+++
@@ -1,9 +0,0 @@
-import { seedCourseData } from "./seed/seed-course-data";
-import { seedUserData } from "./seed/seed-prof-data";
-
-async function main() {
-	await seedUserData();
-	await seedCourseData();
-}
-
-main();
