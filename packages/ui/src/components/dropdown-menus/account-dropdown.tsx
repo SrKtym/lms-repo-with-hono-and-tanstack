4e3c86ff6376952e9a5cdec6b8f55daf748d12de
SrKtym <frmywrkplc@gmail.com>
2026-08-01 17:33:35 +0900

    fix: 微修正

--- packages/ui/src/components/dropdown-menus/account-dropdown.tsx
+++ packages/ui/src/components/dropdown-menus/account-dropdown.tsx
@@ -2,18 +2,23 @@ import { Dropdown } from "@heroui/react";
 import type React from "react";
 import { useState } from "react";
 import type { LinkComponentProps } from "../../lib/utils";
+import { DefaultAvatar } from "../avatar";
 import { CancelButton, DefaultButton, DropdownButton } from "../button";
 import { ControlledModal } from "../modals/controlled-modal";
 
 interface DropdownMenuForAccountProps {
-	children: React.ReactNode;
+	email: string;
+	name: string;
+	image?: string | null;
 	LinkComponent: React.ComponentType<LinkComponentProps>;
 	onLogout: () => void;
 }
 
-// Account dropdown menu
+// アカウントドロップダウンメニュー
 export function DropdownMenuForAccount({
-	children,
+	email,
+	name,
+	image,
 	LinkComponent,
 	onLogout,
 }: DropdownMenuForAccountProps) {
@@ -27,10 +32,16 @@ export function DropdownMenuForAccount({
 	return (
 		<>
 			<Dropdown>
-				<DropdownButton>{children}</DropdownButton>
+				<DropdownButton>
+					<DefaultAvatar src={image} userName={name} />
+					<div className="flex flex-col text-start text-foreground max-sm:hidden">
+						<h3>{name}</h3>
+						<p className="text-gray-500 text-sm dark:text-gray-400">{email}</p>
+					</div>
+				</DropdownButton>
 				<Dropdown.Popover placement="bottom end">
 					<Dropdown.Menu>
-						<Dropdown.Section>
+						<Dropdown.Section className="max-sm:text-lg">
 							{itemList.map(({ key, label }) => {
 								switch (key) {
 									case "profile":
