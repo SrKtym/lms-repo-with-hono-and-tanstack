3d67ca0485da4f799a0425d3f4b8356398fd9483
SrKtym <frmywrkplc@gmail.com>
2026-08-19 13:39:41 +0900

    feat: ユーザーロール（student, professor, admin）に応じたアプリへのアクセス制御を実装

--- packages/auth/src/auth-client.ts
+++ packages/auth/src/auth-client.ts
@@ -2,11 +2,19 @@ import { passkeyClient } from "@better-auth/passkey/client";
 import { env } from "@lms-repo/env/web";
 import { adminClient, twoFactorClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import { ac, roles } from "./permissions";
 
 export const authClient = createAuthClient({
 	baseURL: env.VITE_SERVER_URL,
-	plugins: [passkeyClient(), adminClient(), twoFactorClient()],
+	plugins: [
+		passkeyClient(),
+		adminClient({
+			ac,
+			roles,
+		}),
+		twoFactorClient(),
+	],
 });
 
 type User = typeof authClient.$Infer.Session.user;
-export type UserData = Pick<User, "email" | "name" | "image">;
+export type UserData = Pick<User, "email" | "name" | "image" | "role">;
