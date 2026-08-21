0f9dce4c59bad2c78a39513324fcde24f62a4dd2
SrKtym <frmywrkplc@gmail.com>
2026-08-22 00:36:50 +0900

    feat: 教員情報の登録と取得する機能を実装

---
+++ packages/db/src/utils/mutation/professors.ts
@@ -0,0 +1,31 @@
+import { eq } from "drizzle-orm";
+import { db } from "../..";
+import { departments, professors } from "../../schema";
+
+// 教授データ登録
+export async function registerProfData(userId: string, departmentName: string) {
+	try {
+		await db.transaction(async (tx) => {
+			const [department] = await tx
+				.select({ id: departments.id })
+				.from(departments)
+				.where(eq(departments.name, departmentName))
+				.limit(1);
+
+			if (!department) {
+				return { message: "学科が見つかりません。", status: 404 };
+			}
+
+			await tx
+				.insert(professors)
+				.values({
+					id: userId,
+					departmentId: department.id,
+				})
+				.onConflictDoNothing();
+		});
+		return { message: "学生の所属登録に成功しました。", status: 201 };
+	} catch {
+		return { message: "学生の所属登録に失敗しました。", status: 500 };
+	}
+}
